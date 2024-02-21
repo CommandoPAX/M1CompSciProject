7636e7999cfab3d435586cae6fa52a4513fb8811
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 09:28:49 +0100

    CI

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -7,7 +7,9 @@ from tkinter import*
 from math import*
 import sys
 
-gamma = 5/3
+gamma = 1.4
+L = 1 #longueur du tube en mètres
+n_cell = 10000
 
 def P(rho, K) :
     global gamma
@@ -26,4 +28,15 @@ def F(U_):
     return np.array([U_[0], 0.5*(3-gamma)*U_[1]**2/U[0]+(gamma-1)*U_[2], U_[1]/U_[0]*U_[2]-0.5*(gamma-1)*U_[1]**3/U_[0]**2])
 
 if __name__ == "__main__" :
-    pass
+
+    # Conditions initiales
+
+    X = np.linspace(0,1,num=n_cell)
+
+    rho = np.ones(n_cell)
+    u=np.zeros(n_cell)
+    P = np.ones(n_cell)
+
+    rho[n_cell//2:] = 0.125
+    P[n_cell//2:] = 0.1
+
