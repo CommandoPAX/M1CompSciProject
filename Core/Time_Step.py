e3839200368fde2e36d59e0c11296d8ee0161c54
CommandoPAX <bruant.theo@gmail.com>
2024-02-21 11:07:45 +0100

    Added a simple time step function

--- Core/Time_Step.py
+++ Core/Time_Step.py
@@ -1,4 +1,15 @@
 # Handles fixing the time step for each generation
 
-# Requirements :
-# - Should output a single value
+import numpy as np
+from Core.Config_Loader import *
+from Core.Variables import *
+
+
+def delta_t(u : np.array, a : np.array, dx : float) :
+    # u is an array of the velocity for each cell
+    # a is an array of the sound speed for each cell 
+    # dx is the distance between two cells
+    
+    config = Config_Loader()
+    S_max= np.max(np.absolute(u) + a)
+    return (config.DATA["C"] * dx)/(S_max)
