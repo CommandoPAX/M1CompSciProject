dcb2a49bedd7382dcda25bd28e3f124e16c8b3e9
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 09:06:21 +0100

    formules

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -1,1 +1,29 @@
 # Main file to execute rest of the code
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tkinter import*
+from math import*
+import sys
+
+gamma = 5/3
+
+def P(rho, K) :
+    global gamma
+    return K * np.pow(rho,gamma)
+
+def a (P, rho):
+    global gamma
+    return np.sqrt(gamma*P/rho)
+
+def U(rho, u, P):
+    global gamma
+    return np.array([rho, rho*u, 0.5*rho*u**2+P/(gamma-1)])
+
+def F(U_):
+    global gamma
+    return np.array([U_[0], 0.5*(3-gamma)*U_[1]**2/U[0]+(gamma-1)*U_[2], U_[1]/U_[0]*U_[2]-0.5*(gamma-1)*U_[1]**3/U_[0]**2])
+
+if __name__ == "__main__" :
+    pass
