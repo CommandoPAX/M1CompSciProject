4f33e8a833fa6fa0430be4b4ceaf146facc77ece
CommandoPAX <bruant.theo@gmail.com>
2024-02-21 19:32:20 +0100

    Optimization

--- Core/Physics.py
+++ Core/Physics.py
@@ -6,6 +6,7 @@ from Core.Config_Loader import Config_Loader
 config = Config_Loader()
 gamma = config.DATA["gamma"]
 n_cell = config.DATA["n_cell"]
+C_Var = config.DATA["C"]
 
 def P_(rho : np.array, K : float) :
     """ 
@@ -59,7 +60,6 @@ def W_(rho : np.array, u : np.array, P : np.array):
     Arr[:, 1] = u 
     Arr[:, 2] = P 
     return Arr
-    #return np.array([rho, u, P])
     
 def delta_t(u : np.array, a : np.array, dx : float) :
     """
@@ -68,15 +68,17 @@ def delta_t(u : np.array, a : np.array, dx : float) :
     - a is an array of the sound speed for each cell 
     - dx is the distance between two cells
     """
-    config = Config_Loader()
+    global C_Var
+    
     S_max= np.max(np.absolute(u) + a)
-    return (config.DATA["C"] * dx)/(S_max) # Returns the time step value to be used by Conservative_State_Solver.py
+    return (C_Var * dx)/(S_max) # Returns the time step value to be used by Conservative_State_Solver.py
     
 def derivee (f, x : np.array):
     """ 
     Computes a simple derivative using transmissive boundary conditions
     """
-    config = Config_Loader()
+    global n_cell 
+    
     y = f(x)
     dx = x[1]-x[0]
     yp = np.zeros(n_cell)
