28d092b0adf99d6917491958d8b9fa464c2d2a88
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 10:02:47 +0100

    Graphes

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -46,6 +46,44 @@ def F_(U):
 def W_(rho, u, P):
     return np.array([rho, u, P])
 
+def graphes (X, W, output = "GRAPHIQUE"):
+
+    fig = plt.figure()
+
+    axes = fig.add_subplot(221)
+    axes.set_xlabel("X [m]")
+    axes.set_ylabel("rho")
+
+    axes.plot(X, W[0], label = "Density")
+    axes.legend()
+
+    axes = fig.add_subplot(222)
+    axes.set_xlabel("X [m]")
+    axes.set_ylabel("u [m/s]")
+
+    axes.plot(X, W[1], label="velocity")
+    axes.legend()
+
+
+    axes = fig.add_subplot(223)
+    axes.set_xlabel("X [m]")
+    axes.set_ylabel("P")
+
+    axes.plot(X, W[2], label="Pressure")
+    axes.legend()
+
+    axes = fig.add_subplot(224)
+    axes.set_xlabel("X [m]")
+    axes.set_ylabel("U2")
+
+    U = U_(W[0],W[1],W[2])
+
+    axes.plot(X,U[2], label="Internal energy")
+    axes.legend()
+    plt.savefig("Graphes/"+output)
+
+    plt.show()
+
 if __name__ == "__main__" :
 
     # Conditions initiales
@@ -60,3 +98,5 @@ if __name__ == "__main__" :
     P[n_cell//2:] = 0.1
 
     W = W_ (rho, u, P)    
+
+    graphes(X,W)
