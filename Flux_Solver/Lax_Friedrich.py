17757096f9309527d7c2851974f22930f370b549
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 16:52:14 +0100

    bugs

--- Flux_Solver/Lax_Friedrich.py
+++ Flux_Solver/Lax_Friedrich.py
@@ -3,14 +3,19 @@
 import numpy as np
 from Core.Config_Loader import Config_Loader
 from Core.Error_Handler import LogError
+from Core.Conservative_State_Solver import*
+
+config = Config_Loader()
 
 def F12_Friedrich (U, X, dt):
-    global n_cell
-    R = np.zeros(n_cell,3)
+
+    n_cell = config.DATA["n_cell"]
+
+    R = np.zeros((3,n_cell))
     dx = X[1]-X[0]
 
-    print(R.shape())
+    print(R[0].shape)
 
     for i in range(3):
         for j in range(n_cell) :
-            R[i][j] = 0.5*(F(U[i][j])+F(U[i][j+1]))+0.5*dx/dt*(U[i][j]-U[i][j+1])
+            R[i][j] = 0.5*(F_(U[i][j])+F_(U[i][j+1]))+0.5*dx/dt*(U[i][j]-U[i][j+1])
