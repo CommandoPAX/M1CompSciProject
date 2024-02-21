77a95e1e834ba1dfe7517224cc92f5b73e2c820e
Iornaorn <aaron.08.w@gmail.com>
2024-02-21 17:43:14 +0100

    lol

--- Flux_Solver/Lax_Wendroff.py
+++ Flux_Solver/Lax_Wendroff.py
@@ -11,8 +11,15 @@ def F12_Friedrich (U, dx, dt):
     
     n_cell = config.DATA["n_cell"]
     c=0.9
+    R1=np.zeros((3,n_cell))
+    R2=np.zeros((3,n_cell))
     R=np.zeros((3,n_cell))
     
+    
     for i in range(n_cell):
-        R[:,i]=0.5*(U[:,i]+U[:,i+1]) + 0.5*(dt/dx)*(F_[:,i]-F_[:,i+1]) #R[:,i]=U[:,i+1/2] at t'=t+1/2*dt
+        R1[:,i]=0.5*(U[:,i]+U[:,i+1]) - 0.5*(dt/dx)*(F_(U[:,i+1])-F_(U[:,i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
+        R2[:,i]=0.5*(U[:,i]+U[:,i-1]) - 0.5*(dt/dx)*(F_(U[:,i])-F_(U[:,i-1])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
+        R[:,i]=U[:,i]-(dt/dx)*(F_(R1[:,i]-F_(R2[:,i]))) #R[:,i]=U[:,i] at t'=t+dt
+        
+    return R
         
