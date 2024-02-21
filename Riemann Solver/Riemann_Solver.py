7f563edfd7e396867f530a5c9466f35ba42b52c4
CommandoPAX <bruant.theo@gmail.com>
2024-02-21 13:22:59 +0100

    Added docs

--- Riemann Solver/Riemann_Solver.py
+++ Riemann Solver/Riemann_Solver.py
@@ -1,5 +1,5 @@
 # Exact Riemann solver
 
 # Requirements : 
-# - Should check the config files for problems conditions (json files)
+# - Should check the config files for problems conditions (use Config_Loader.py)
 # - Should output something usable by the Plot_Handler function 
