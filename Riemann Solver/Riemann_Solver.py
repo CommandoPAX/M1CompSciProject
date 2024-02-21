22a9cd9a786e3f8ad0d34ad481e6118809f10e52
Iornaorn <aaron.08.w@gmail.com>
2024-02-21 16:28:30 +0100

    so long kinga bowser

--- Riemann Solver/Riemann_Solver.py
+++ Riemann Solver/Riemann_Solver.py
@@ -1,5 +1,4 @@
-# Exact Riemann solver
-
+# E#Sata antagi
 # Requirements : 
 # - Should check the config files for problems conditions (use Config_Loader.py)
 # - Should output something usable by the Plot_Handler function 
