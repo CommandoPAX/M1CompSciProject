74e6e4516fc4edb0d24698c874368e2c4107f53d
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 19:17:58 +0100

    global

--- Core/Physics.py
+++ Core/Physics.py
@@ -43,8 +43,7 @@ def F_(U : np.array):
     """ 
     Computes the flux function as defined on page 1 of the subject
     """
-    config = Config_Loader()
-    gamma = config.DATA["gamma"]
+    global gamma
     
     return np.array([U[0], 0.5*(3-gamma)*U[1]**2/U[0]+(gamma-1)*U[2], U[1]/U[0]*U[2]-0.5*(gamma-1)*U[1]**3/U[0]**2])
 
