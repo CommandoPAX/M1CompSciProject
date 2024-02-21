178f488b6acd0a0f8c2d39f9b32a206ac3d73e87
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 20:10:43 +0100

    tests

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -53,7 +53,7 @@ if __name__ == "__main__" :
     dx = X[1]-X[0]
     while 1 :
         Ttot += delta_t(U[:, 1], a_(U[:, 2], U[:, 0]),dx)
-        print(Ttot)
+        #print(Ttot)
         U = U_next(U,dx)
         if Ttot > 0.25 : break
 
