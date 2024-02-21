194e09d1c968efabd83d16b9286c4dc84b317200
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-21 09:47:18 +0100

    crochet

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -44,7 +44,7 @@ def F_(U):
     return np.array([U[0], 0.5*(3-gamma)*U[1]**2/U[0]+(gamma-1)*U[2], U[1]/U[0]*U[2]-0.5*(gamma-1)*U[1]**3/U[0]**2])
 
 def W_(rho, u, P):
-    return np.array(rho, u, P)
+    return np.array([rho, u, P])
 
 if __name__ == "__main__" :
 
