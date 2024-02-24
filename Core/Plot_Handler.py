c98810cb5e1b6591cd583512dbf23fdc6f3e6afc
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-24 17:37:49 +0100

    correction graphe

--- Core/Plot_Handler.py
+++ Core/Plot_Handler.py
@@ -2,6 +2,10 @@
 
 import matplotlib.pyplot as plt 
 from Core.Physics import *
+from Core.Config_Loader import Config_Loader
+
+config = Config_Loader()
+gamma = config.DATA["gamma"]
 
 def Create_Plots(X : np.array, W : np.array, output : str = "GRAPHIQUE"):
     """ 
@@ -10,35 +14,41 @@ def Create_Plots(X : np.array, W : np.array, output : str = "GRAPHIQUE"):
     All plots are in function of X[m]
     """
     
+    global gamma
+
     fig = plt.figure()
+
+    rho = W[:,0]
+    u = W[:,1]
+    P = W[:,2]
+    U_int = 0.5*rho*u**2+P/(gamma-1)
 
     # Density
     axes = fig.add_subplot(221)
     axes.set_xlabel("X [m]")
     axes.set_ylabel("rho")
-    axes.plot(X, W[:, 0], label = "Density")
+    axes.plot(X, rho, label = "Density")
     axes.legend()
 
     # Velocity
     axes = fig.add_subplot(222)
     axes.set_xlabel("X [m]")
     axes.set_ylabel("u [m/s]")
-    axes.plot(X, W[:, 1], label="velocity")
+    axes.plot(X, u, label="velocity")
     axes.legend()
     
     # Pressure
     axes = fig.add_subplot(223)
     axes.set_xlabel("X [m]")
     axes.set_ylabel("P")
-    axes.plot(X, W[:, 2], label="Pressure")
+    axes.plot(X, P, label="Pressure")
     axes.legend()
     
     # Internal Energy
     axes = fig.add_subplot(224)
     axes.set_xlabel("X [m]")
     axes.set_ylabel("U2")
-    U = U_(W[:,0],W[:,1],W[:,2])
-    axes.plot(X,U[:,2], label="Internal energy")
+    axes.plot(X,U_int, label="Internal energy")
     axes.legend()
     
     # Saves and shows the plots
