2704cec0c596f14683bb6b63cc6999b9ec9a5535
Iornaorn <aaron.08.w@gmail.com>
2024-02-28 16:44:06 +0100

    lol

--- Flux_Solver/Lax_Wendroff.py
+++ Flux_Solver/Lax_Wendroff.py
@@ -10,21 +10,21 @@ def F12_Wendroff (U : np.array, dx, dt : float):
     
     n_cell = config.DATA["n_cell"]
     
-    R1=np.zeros((3,n_cell))
-    R2=np.zeros((3,n_cell))
-    R=np.zeros((3,n_cell))
+    R1=np.zeros((n_cell,3))
+    R2=np.zeros((n_cell,3))
+    R=np.zeros((n_cell,3))
     
     for i in range(n_cell-1):
         if i==0:
-            R1[:,i]=0.5*(U[:,i]+U[:,i+1]) - 0.5*(dt/dx)*(F_(U[:,i+1])-F_(U[:,i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
-            R2[:,i]=0.5*(U[:,i]+U[:,i]) - 0.5*(dt/dx)*(F_(U[:,i])-F_(U[:,i])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
+            R1[i]=0.5*(U[i]+U[i+1]) - 0.5*(dt/dx)*(F_(U[i+1])-F_(U[i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
+            R2[i]=0.5*(U[i]+U[i]) - 0.5*(dt/dx)*(F_(U[i])-F_(U[i])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
         elif i==n_cell-1:
-            R1[:,i]=0.5*(U[:,i]+U[:,i]) - 0.5*(dt/dx)*(F_(U[:,i])-F_(U[:,i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
-            R2[:,i]=0.5*(U[:,i]+U[:,i-1]) - 0.5*(dt/dx)*(F_(U[:,i])-F_(U[:,i-1])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
+            R1[i]=0.5*(U[i]+U[i]) - 0.5*(dt/dx)*(F_(U[i])-F_(U[i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
+            R2[i]=0.5*(U[i]+U[i-1]) - 0.5*(dt/dx)*(F_(U[i])-F_(U[i-1])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
         else:    
-            R1[:,i]=0.5*(U[:,i]+U[:,i+1]) - 0.5*(dt/dx)*(F_(U[:,i+1])-F_(U[:,i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
-            R2[:,i]=0.5*(U[:,i]+U[:,i-1]) - 0.5*(dt/dx)*(F_(U[:,i])-F_(U[:,i-1])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
-        R[:,i]=U[:,i]-(dt/dx)*(F_(R1[:,i]-F_(R2[:,i]))) #R[:,i]=U[:,i] at t'=t+dt
+            R1[i]=0.5*(U[i]+U[i+1]) - 0.5*(dt/dx)*(F_(U[i+1])-F_(U[i])) #R1[:,i]=U[:,i+1/2] at t'=t+1/2*dt
+            R2[i]=0.5*(U[i]+U[i-1]) - 0.5*(dt/dx)*(F_(U[i])-F_(U[i-1])) #R2[:,i]=U[:,i-1/2] at t'=t+1/2*dt
+        R[i]=U[i]-(dt/dx)*(F_(R1[i]-F_(R2[i]))) #R[:,i]=U[:,i] at t'=t+dt
         
     return R
         
