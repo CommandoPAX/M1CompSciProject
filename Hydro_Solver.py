9aed83406ddf3cf22956752bd6c55799faf24072
Iornaorn <aaron.08.w@gmail.com>
2024-02-28 16:33:12 +0100

    mdr

--- Hydro_Solver.py
+++ Hydro_Solver.py
@@ -54,7 +54,7 @@ if __name__ == "__main__" :
     while 1 :
         Ttot += delta_t(U[:, 1], a_(U[:, 2], U[:, 0]),dx)
         #print(Ttot)
-        U = U_next(U,dx)
+        U = U_next(U,dx,"LW")
         if Ttot > 0.25 : break
 
     # TEST
