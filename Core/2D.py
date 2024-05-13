a9a45e4ecf6260ece99f9eb1c64b128913cc24b9
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-05-13 16:01:55 +0200

    2D

---
+++ Core/2D.py
@@ -0,0 +1,9 @@
+import numpy as np
+from Core.Physics import *
+from Core.Plot_Handler import *
+from Core.Config_Loader import Config_Loader
+from Flux_Solver.Lax_Friedrich import*
+from Core.Conservative_State_Solver import*
+from Core.Riemann_Solver import *
+
+pass
