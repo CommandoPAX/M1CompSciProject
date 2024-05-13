ee95efd99bbdd8e436c9b6d1a70b2b48da31cb0f
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-05-13 16:07:38 +0200

    2D

--- Core/2D.py
+++ Core/2D.py
@@ -6,4 +6,13 @@ from Flux_Solver.Lax_Friedrich import*
 from Core.Conservative_State_Solver import*
 from Core.Riemann_Solver import *
 
-pass
+n_cell = 25
+
+rho = np.ones((n_cell,n_cell))
+ux = np.zeros((n_cell,n_cell))
+uy = np.zeros((n_cell,n_cell))
+P = np.ones((n_cell,n_cell))
+
+P[10,10] = 10
+
+plt.imshow(P)
